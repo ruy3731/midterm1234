65d1c68809441fce536c192b0d5432f90e48ede4
Ruy "Lorde Supremo " Oliveira <rsilvagomesdeol3731@conestogac.on.ca>
2022-10-21 18:32:23 -0400

    Styles

---
+++ Script/script.js
@@ -0,0 +1,11 @@
+
+function order()
+{
+    var modal = document.getElementById("orderBox");
+    modal.style.display = "block";
+}
+function closeMsg()
+{
+    var modal = document.getElementById("orderBox");
+    modal.style.display = "none";
+}
